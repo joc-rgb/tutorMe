1bab4160cf1bcec815b1bdd4ee17751f9a538f9f
jocxy <jocelinxy@gmail.com>
2023-03-07 16:57:35 +0800

    Run `npx prisma migrate dev --name init` | Migrating and pushing changes to the database

---
+++ prisma/migrations/20230307072157_init/migration.sql
@@ -0,0 +1,42 @@
+-- CreateEnum
+CREATE TYPE "Mode" AS ENUM ('ONLINE', 'IN_PERSON');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+    "location" TEXT NOT NULL,
+    "profileImg" TEXT,
+    "phoneNumber" TEXT,
+    "about" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Offer" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "tag" TEXT[],
+    "pricePerSession" DOUBLE PRECISION[],
+    "tutorMode" "Mode"[],
+    "contact" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "postedById" INTEGER NOT NULL,
+
+    CONSTRAINT "Offer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Offer" ADD CONSTRAINT "Offer_postedById_fkey" FOREIGN KEY ("postedById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/migrations/20230307082810_init/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "expertiseIn" TEXT,
+ADD COLUMN     "highestEducationLvl" TEXT;
