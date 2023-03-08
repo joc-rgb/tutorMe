ab58c6b2f81765f0349a1da324688d0258bda624
jocxy <jocelinxy@gmail.com>
2023-03-08 15:42:34 +0800

    Update Yogo Server

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,9 +1,8 @@
-import { createYoga, createSchema } from "graphql-yoga";
-import { resolvers } from "../../graphql/resolvers";
-import { typeDefs } from "../../graphql/schema";
+import { createYoga} from "graphql-yoga";
+import { schema } from "../../graphql/schema";
 
 export default createYoga<{req:Request, res:Response}>({
-    schema:createSchema({typeDefs, resolvers}),
+    schema,
     graphqlEndpoint: '/api/graphql'
 })
 
