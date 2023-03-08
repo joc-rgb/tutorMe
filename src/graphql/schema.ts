637945758256ad8aa512babc95f7a6b8086163f1
jocxy <jocelinxy@gmail.com>
2023-03-08 15:41:00 +0800

    Change manual typed typeDefs to pothos generated

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,14 +1,19 @@
-export const typeDefs = `
-    type User {
-        username: String
-        email: String
-        location: String
-        about: String
-        expertiseIn: [String]
-        highestEducationLvl: String
-    }
+import { builder } from "./builder";
 
-    type Query{
-        users: [User]!
-    }
-`
+export const schema = builder.toSchema()
+
+//Pothos generate GraphQL schema for Us!
+// export const typeDefs = `
+//     type User {
+//         username: String
+//         email: String
+//         location: String
+//         about: String
+//         expertiseIn: [String]
+//         highestEducationLvl: String
+//     }
+
+//     type Query{
+//         users: [User]!
+//     }
+// `
