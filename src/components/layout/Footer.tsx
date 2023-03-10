5068556655035af474e37482730ed30d290e290d
jocxy <jocelinxy@gmail.com>
2023-03-10 12:16:10 +0800

    Fix TailwindCSS not working bug

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,7 +9,8 @@ const Footer = () => {
   return (
     
 <footer className="p-4 bg-white border-t-2 b-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 ">
-    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="https://github.com/joc-rgb" className="hover:underline">Jocelin RGB</a>
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="https://github.com/joc-rgb" className="hover:underline" target="_blank"
+          rel="noopener noreferrer">Jocelin RGB</a>
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0 gap-3">
         {links.map(({ href, label }) => (
