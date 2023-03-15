8dfc1ed6a48aa014f4f067ff97846c935a1ec48a
jocxy <jocelinxy@gmail.com>
2023-03-15 16:39:16 +0800

    Set Up 0Auth, Apollo Client & React Hot Toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,14 @@ import { ApolloProvider } from '@apollo/client'
 import apolloClient from '../lib/apollo'
 import Layout from '../components/layout/Layout'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
+import { Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <ApolloProvider client={apolloClient}>
         <Layout>
+          <Toaster/>
           <Component {...pageProps} />
         </Layout>
       </ApolloProvider>
