bc639da252729ad607b73fbe1931ac90c35e565b
jocxy <jocelinxy@gmail.com>
2023-03-17 12:50:13 +0800

    Define a Date scalar type

--- src/graphql/builder.ts
+++ src/graphql/builder.ts
@@ -2,15 +2,19 @@ import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from "@pothos/plugin-prisma";
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
 import prisma from "../lib/prisma";
-import { createContext } from "./context";
+import { createContext } from "./context"; 
+import { DateResolver } from "graphql-scalars";
 
-export const builder = new SchemaBuilder<{ PrismaTypes: PrismaTypes,Context: ReturnType<typeof createContext> }>({
+export const builder = new SchemaBuilder<{ PrismaTypes: PrismaTypes,Context: ReturnType<typeof createContext>, Scalars: {
+  Date: { Input: Date; Output: Date };
+}; }>({
     plugins: [PrismaPlugin],
     prisma: {
         client: prisma,
     },
+    
 })
-
+builder.addScalarType("Date", DateResolver, {});
 builder.queryType({
     fields: (t) => ({
       ok: t.boolean({
