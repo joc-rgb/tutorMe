7e05465317951fb4df7edc0760e949bc26aabfed
jocxy <jocelinxy@gmail.com>
2023-03-21 12:57:14 +0800

    Update offer to post

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -2,7 +2,7 @@ import prisma from "../lib/prisma"
 export const resolvers = {
     Query:{
         offers:()=>{
-            return prisma.offer.findMany()
+            return prisma.post.findMany()
         }
     }
 }
