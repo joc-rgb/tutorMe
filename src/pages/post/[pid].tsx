a9eb2775d050a439cbdc42265ba7197269bd82ef
jocxy <jocelinxy@gmail.com>
2023-03-26 21:35:32 +0800

    Create post page

---
+++ src/pages/post/[pid].tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useRouter } from 'next/router';
+import { postQuery, similarPostQuery } from '../../graphql/operations/post';
+import { useQuery } from '@apollo/client';
+import Loader from '../../components/global/Loader';
+import { Post } from '@prisma/client';
+import PostDetail from '../../components/post/PostDetail';
+import SuggestedPosts from '../../components/post/SuggestedPosts';
+
+const PostPage = () => {
+    const postId = useRouter().query.pid
+    console.log(postId)
+    const { loading, error, data} = useQuery(postQuery, {
+        variables: { id: parseInt(postId as string) as number}
+      });
+
+      if (loading) {
+        return (
+          <div className="flexStart min-h-screen">
+            <Loader className='w-96 h-96' />
+          </div>
+        );
+      }
+      if (!loading&&!data) {
+        return <p>Error 404</p>;
+      }
+      const post:Post = data.post
+    return (
+      <div className="flex flex-col gap-10">
+      <PostDetail post={post} />
+      <SuggestedPosts postId={post.id} tag={post.tag[0]}/> 
+    </div>
+      
+    )
+}
+
+export default PostPage
