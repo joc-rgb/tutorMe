6aff99c75130a30a781276cf61117d2921a660ac
jocxy <jocelinxy@gmail.com>
2023-03-26 15:44:44 +0800

    Add domain for next/image

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images:{
-    domains:['placekitten.com','tutorme.s3.ap-southeast-1.amazonaws.com']
+    domains:['placekitten.com','tutorme.s3.ap-southeast-1.amazonaws.com','tutorme.s3.amazonaws.com']
   }
 }
 
