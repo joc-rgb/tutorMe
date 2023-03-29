dc0690f6b842000112a74bd3bf3c23eec98e4721
jocxy <jocelinxy@gmail.com>
2023-03-29 22:13:57 +0800

    Create Error Component

---
+++ src/components/global/Error.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+import Button, { Variant } from './Button'
+
+interface IError{
+    errorText?: string
+    errorMsg?:string
+}
+const Error = (props:IError) => {
+  return (
+    <div className='w-full max-h-[100vh] flex flex-col gap-4 items-center justify-center p-20'>
+        <p className="text-6xl font-bold">{props.errorText || 'Unknown Error'}</p>
+        <p className="text-lg">{props.errorMsg || 'Something&apos;s wrong'}</p>
+        <Link href={'/'} >
+            <Button variants={Variant.outline}>Back To Home Page</Button>
+        </Link>
+    </div>
+  )
+}
+
+export default Error
