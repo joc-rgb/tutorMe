68774da7006a47038528ef6b3e3a02b1e8174711
jocxy <jocelinxy@gmail.com>
2023-03-30 14:10:02 +0800

    Create User Page

--- src/pages/user/[uid].tsx
+++ src/pages/user/[uid].tsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import UserDetails from '../../components/post/UserDetails'
+import { GetServerSideProps } from 'next'
+import { getSession } from '@auth0/nextjs-auth0'
+import { IPost } from '../../constant'
 
-const UserPage = () => {
+interface IUserPage{
+  id: number;
+  name: string;
+  email: string;
+  location: string | null;
+  profileImg: string | null;
+  phoneNumber: string | null;
+  about: string | null;
+  expertiseIn: string[];
+  highestEducationLvl: string | null;
+  posts: IPost[] 
+}
+const UserPage = (user:IUserPage) => {
+  console.log(user)
   return (
-    <div>UserPage</div>
+    <UserDetails posts={user.posts} userInfo={user} />
   )
 }
 
 export default UserPage
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res, query } ) => {
+  const userId = query.uid as string
+  const session = await getSession(req, res);
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/api/auth/login',
+      },
+      props: {},
+    };
+  }
+  
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(userId)
+    },
+    select: {
+      id:true,
+      email:true,
+      phoneNumber:true,
+      about:true,
+      expertiseIn:true,
+      location:true,
+      profileImg:true,
+      highestEducationLvl: true,
+      posts:true
+    },
+  });
+  
+  if (!user) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/404',
+      },
+      props: {},
+    };
+  }
+  return {
+    props: user,
+  };
+};
